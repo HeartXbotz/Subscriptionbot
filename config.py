0ffb9b49838f6e70005a7d57c50d918a5cd6654b
vijaybots <vijayprakashv358@gmail.com>
2025-09-10 21:54:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
+API_ID = int(os.getenv("28455032"))
+API_HASH = os.getenv("28dbb18229d7701a856c42a46083cccf")
+BOT_TOKEN = os.getenv("7931356250:AAEl8s6RwaIh_Ek44u0-1JcX_lgthgITwCE")
+MONGO_URI = os.getenv("mongodb+srv://kuttycloudbot:12@obitoleech.y3n6szj.mongodb.net/?retryWrites=true&w=majority&appName=Obitoleech")
 
 # Multiple admin IDs separated by space
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split()))
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "1572929036").split()))
 
 # Optional log channel
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1002463259408))
 
 # Example: private group or channel where premium users are added
-PREMIUM_CHAT_ID = int(os.getenv("PREMIUM_CHAT_ID", 0))
+PREMIUM_CHAT_ID = int(os.getenv("PREMIUM_CHAT_ID", -1002854028155))
